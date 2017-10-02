857cbf968ba72e0dc41a52eaadd5c67229e06b8c
Savalandr <32355527+Savalandr@users.noreply.github.com>
2017-10-02 14:48:00 +0300

    V1.0

---
+++ complex/complex_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <sstream> 
+#include "complex.h"
+
+int main()
+{
+	using namespace std;
+
+	Complex z;
+	z += Complex(8.0);
+	testParse("{8.9,9}");
+	testParse("{8.9, 9}");
+	testParse("{8.9,9");
+	return 0;
+}
