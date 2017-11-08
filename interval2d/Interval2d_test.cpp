0e32c2c2c7d4bb380e3a64f9d48f6b25af2e4071
Savalandr <32355527+Savalandr@users.noreply.github.com>
2017-11-08 23:01:44 +0300

    V1.7.2

---
+++ interval2d/Interval2d_test.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <sstream> 
+#include "Interval2d.h"
+using namespace std;
+
+int main()
+{
+	Interval2d a(3, 4);
+	Interval2d b(2, 3);
+	Interval2d a1(3, 4);
+	Interval2d b1(2, 3);
+
+	a1 += 5;
+	b1 -= 2;
+
+	Interval2d s;
+	Interval2d z;
+
+	s = a + b;
+	z = a - b;
+
+	cout << a1.x <<"_"<< a1.y << endl;
+	cout << b1.x << "_" << b1.y << endl;
+	cout << s.x << "_" << s.y << endl;
+	cout << z.x << "_" << z.y << endl;
+
+	int ThatsAll;
+		cin >> ThatsAll;
+
+	return 0;
+}
